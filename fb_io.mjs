52155aa81b1fd7e62ef4f1c55da0c47e0a7f56be
22033cc <22033cc@hvhs.school.nz>
2025-05-01 23:01:34 +0000

    authenticate

--- fb_io.mjs
+++ fb_io.mjs
@@ -1,7 +1,7 @@
 //**************************************************************/
 // fb_io.mjs
 // Generalised firebase routines
-// Written by <Your Name Here>, Term 2 202?
+// Written by Conor Church, Term 2 2025
 //
 // All variables & function begin with fb_  all const with FB_
 // Diagnostic code lines have a comment appended to them //DIAG
@@ -15,20 +15,27 @@ console.log('%c fb_io.mjs',
 // Import all external constants & functions required
 /**************************************************************/
 // Import all the methods you want to call from the firebase modules
+import { initializeApp }
+    from "https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js";
 
+import { getDatabase }
+    from "https://www.gstatic.com/firebasejs/9.6.1/firebase-database.js";
+    
+import { getAuth, GoogleAuthProvider, signInWithPopup }
+    from "https://www.gstatic.com/firebasejs/9.6.1/firebase-auth.js"; 
 
 /**************************************************************/
 // EXPORT FUNCTIONS
 // List all the functions called by code or html outside of this module
 /**************************************************************/
 export { 
-    fb_initialise 
+    fb_initialise, fb_authenticate
 };
 
 /***************************************************************
 // function fb_intitialise()
 // called by html button "fb_initialise()"
-// console logs "fb_intialise()" in orange and white
+// intatilises connecting to firebase
  ****************************************************************/
 function fb_initialise() {
     
@@ -38,6 +45,61 @@ function fb_initialise() {
     console.log("%c galvinise:",
                 'color:'+ COL_B + 
                 '; background-color:' + COL_C + ';');
+    const FB_GAMECONFIG = {
+        apiKey: "AIzaSyCCqhJW7S5L9nSkhlB_8Nvg3zzD4w65hjU",
+        authDomain: "comp-conor-church.firebaseapp.com",
+        databaseURL: "https://comp-conor-church-default-rtdb.asia-southeast1.firebasedatabase.app",
+        projectId: "comp-conor-church",
+        storageBucket: "comp-conor-church.firebasestorage.app",
+        messagingSenderId: "807950196532",
+        appId: "1:807950196532:web:44538dd1b8184ee5760f61",
+        measurementId: "G-G7Z4YR3HX7"
+    };
+    const FB_GAMEAPP = initializeApp(FB_GAMECONFIG)
+    const FB_GAMEDB = getDatabase(FB_GAMEAPP)
+    console.info(FB_GAMEDB);
+}
+
+/***************************************************************
+// function fb_authenticate()
+// called by html button "authenticate"
+// 
+ ****************************************************************/
+function fb_authenticate(){
+console.log('%c authenticate():',
+    'color:' + COL_C +
+    'background-color:' + COL_B + ';');
+    const AUTH = getAuth(); //something is wrong here
+    const PROVIDER = new GoogleAuthProvider();
+    
+    PROVIDER.setCustomParameters({
+        prompt: 'select_account'
+    });
+
+    signInWithPopup(AUTH, PROVIDER).then((result) => {
+        alert("thank you for signing correctly")
+    })
+
+    .catch((error) => {
+        alert("you signed in wrong")
+        console.log(error)
+    });
+}
+/***************************************************************
+// function fb_detectLoginChange()
+// called by html button "detect login change "
+// 
+ ****************************************************************/
+function fb_detectLoginChange(){
+
+}
+/***************************************************************
+// function fb_logOut()
+// called by html button "logout "
+// 
+ ****************************************************************/
+function fb_logOut(){
+
 }
 
 /**************************************************************/
---
+++ fb_record.mjs
@@ -0,0 +1,68 @@
+//**************************************************************/
+// fb_records.mjs
+// firebase routines based on recording information 
+// Written by Conor Church, Term 2 2025
+//
+// All variables & function begin with fb_  all const with FB_
+// Diagnostic code lines have a comment appended to them //DIAG
+/**************************************************************/
+const COL_C = 'white';	    // These two const are part of the coloured 	
+const COL_B = '#CD7F32';	//  console.log for functions scheme
+console.log('%c fb_io.mjs',
+            'color: blue; background-color: white;');
+
+/**************************************************************/
+// Import all external constants & functions required
+/**************************************************************/
+// Import all the methods you want to call from the firebase modules
+
+
+/**************************************************************/
+// EXPORT FUNCTIONS
+// List all the functions called by code or html outside of this module
+/**************************************************************/
+
+/***************************************************************
+// function fb_writeRecord()
+// called by html button "write record"
+// 
+ ****************************************************************/
+function fb_writeRecord(){
+
+}
+/***************************************************************
+// function fb_readRecord()
+// called by html button "read record "
+// 
+ ****************************************************************/
+function fb_readRecord(){
+
+}
+/***************************************************************
+// function fb_readAll()
+// called by html button "readAll "
+// 
+ ****************************************************************/
+function fb_readAll(){
+
+}
+/***************************************************************
+// function fb_updateRecord()
+// called by html button "update record "
+// 
+ ****************************************************************/
+function fb_updateRecord(){
+
+}
+/***************************************************************
+// function fb_updateRecord()
+// called by html button "update record "
+// 
+ ****************************************************************/
+function fb_updateRecord(){
+
+}
+
+/**************************************************************/
+// END OF CODE
+/**************************************************************/
